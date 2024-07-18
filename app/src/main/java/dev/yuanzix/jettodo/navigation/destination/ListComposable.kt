1d7e631ceeb84e2cdc1fe92c0320dbf600b4cfa8
YuanziX <achubadyal4@gmail.com>
2024-07-18 20:52:54 +0530

    listComposable: persist action post orientation change and only set if different
    Signed-off-by: YuanziX <achubadyal4@gmail.com>

--- app/src/main/java/dev/yuanzix/jettodo/navigation/destination/ListComposable.kt
+++ app/src/main/java/dev/yuanzix/jettodo/navigation/destination/ListComposable.kt
@@ -2,12 +2,16 @@ package dev.yuanzix.jettodo.navigation.destination
 
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import dev.yuanzix.jettodo.navigation.ListScreen
 import dev.yuanzix.jettodo.ui.screen.list.ListScreen
 import dev.yuanzix.jettodo.ui.viewmodel.SharedViewModel
+import dev.yuanzix.jettodo.util.Action
 import dev.yuanzix.jettodo.util.toAction
 
 fun NavGraphBuilder.listComposable(
@@ -16,9 +20,15 @@ fun NavGraphBuilder.listComposable(
 ) {
     composable<ListScreen> {
         val action = it.toRoute<ListScreen>().name.toAction()
+        var myAction by rememberSaveable {
+            mutableStateOf(Action.NoAction)
+        }
 
         LaunchedEffect(key1 = action) {
-            sharedViewModel.action.value = action
+            if (action != myAction) {
+                myAction = action
+                sharedViewModel.action.value = action
+            }
         }
 
         val databaseAction by sharedViewModel.action
